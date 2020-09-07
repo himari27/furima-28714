5b3a16aaa61660aeb4527da005f5d37d322a682e
himari27 <madrid99hiro@gmail.com>
2020-09-07 18:08:53 +0900

    itemモデルのバリデーション〜プルダウンの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,24 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
     # 保存できたらルートに飛ぶ、できなかったらnewにもどる
   end
 
   def new
+    @item = Item.new
   end
 
-  # 
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:text,:price,:category_id,:condition_id,:prefecture_id,:shipping_day_id,:shipping_fee_id)
+  end
+
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :prefecture, :shipping_day, :shipping_fee
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :shipping_fee
+ 
+  validates :name, :text, :price, :category, :condition, :prefecture, :shipping_day, :shipping_fee, presence: true
 
-  
 end
