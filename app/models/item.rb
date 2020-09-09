ee599396d8c2197bc92d20c1250680c4e65747e3
himari27 <madrid99hiro@gmail.com>
2020-09-09 18:29:54 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,10 @@ class Item < ApplicationRecord
   has_one_attached :image
  
   validates :name, :text, :category, :condition, :prefecture, :shipping_day, :shipping_fee, presence: true
-  with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
-    validates :price, presence: true
-  end
+  # with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+    validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } 
+    validates :price, :numericality => { :greater_than_or_equal_to => 300 , message:'must be more than 300'}
+    validates :price, :numericality => { :less_than_or_equal_to => 9999999 , message:'must be lower than 9,999,999'}
+
+  # end
 end
