ec630802542e4557d5c8b08f0a0ce35521ad1b71
himari27 <madrid99hiro@gmail.com>
2020-09-14 15:32:36 +0900

    before_actionを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -17,17 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to "/items/#{@item.id}"
+      redirect_to action: :show
     else
       render :edit
     end
@@ -37,5 +37,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :price, :category_id, :condition_id, :prefecture_id, :shipping_day_id, :shipping_fee_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
