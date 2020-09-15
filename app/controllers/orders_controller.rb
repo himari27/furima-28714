83683abaeee93798b8cd91cd9ababc861e19bc74
himari27 <madrid99hiro@gmail.com>
2020-09-15 14:15:13 +0900

    購入画面へのルーティング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,16 @@
 class OrdersController < ApplicationController
 
-def index
-  
-end
+  def index
+  end
 
-def create
-  
-end
+  def create
+  end
 
+  private
 
+  def order_params
+    params.require(:order).permit(:post_code, :city, :address, :building_name, :phone_number, :prefecture_id, :item_purchase_id).merge(user_id: current_user.id)
+  end
 end
 
 
