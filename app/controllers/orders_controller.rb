252d557694ff56fe7f2c03622c47a2deae495c94
himari27 <madrid99hiro@gmail.com>
2020-09-15 15:48:56 +0900

    card.jsの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,29 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:post_code, :city, :address, :building_name, :phone_number, :prefecture_id, :item_purchase_id).merge(user_id: current_user.id)
+    params.require(:order).permit(:price, :tpken, :post_code, :city, :address, :building_name, :phone_number, :prefecture_id, :item_purchase_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_9009f488776dc6c57b6fe3cb"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 end
 
